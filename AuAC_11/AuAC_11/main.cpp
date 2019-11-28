cc9781b894e62d5bf0acd62841bdc942fc66f018
IconLivin <anton.parole@mail.ru>
2019-11-28 21:12:15 +0300

    Added Vitalya's RK matcher, but i guess it's bad :(

--- AuAC_11/AuAC_11/main.cpp
+++ AuAC_11/AuAC_11/main.cpp
@@ -2,6 +2,43 @@
 #include <vector>
 #include <iostream>
 
+uint64_t First_Hash(std::string str, size_t degree)
+{
+	uint64_t hash = 0;
+	for (size_t i = 0; i < str.size(); ++i)
+	{
+		hash += pow(degree, i) * str[i];
+	}
+	return hash;
+}
+
+uint64_t Hash(std::string str, uint64_t prev_hash, size_t degree)
+{
+	prev_hash = (prev_hash - str[0]) / degree + str[str.size() - 1] * pow(degree, static_cast<int>(str.size()) - 2);
+	return prev_hash;
+}
+
+int RK_Match(std::string text, std::string pattern) {
+	size_t rk_const = 2;
+	const uint64_t pattern_hash = First_Hash(pattern, rk_const);
+	uint64_t hash = First_Hash(text.substr(0, pattern.size()), rk_const);
+	if (hash == pattern_hash) {
+		if (!pattern.compare(text.substr(0, pattern.size()))) {
+			return 0;
+		}
+	}
+	for (size_t i = 0; i < static_cast<size_t>(text.size()) - static_cast<size_t>(pattern.size()); ++i) {
+		std::string tmp = text.substr(i, pattern.size() + 1);
+		hash = Hash(tmp, hash, rk_const);
+		if (hash == pattern_hash) {
+			if (!pattern.compare(tmp.substr(1,pattern.size()))) {
+				return i + 1;
+			}
+		}
+	}
+	return -1;
+}
+
 int KMP_Match(std::string text, std::string pattern) {
 	const size_t n = text.length();
 	const size_t m = pattern.length();
@@ -60,7 +97,8 @@ int main() {
 	std::cin >> find_it;
 	int kmp_pos = KMP_Match(text, find_it);
 	int naive_pos = Naive_Match(text, find_it);
-	kmp_pos == naive_pos ? std::cout << "Ok!\n" : std::cout << "Bad :(\n";
+	int rk_pos = RK_Match(text, find_it);
+	std::cout << kmp_pos << std::endl << naive_pos << std::endl << rk_pos << std::endl;
 	
 	return 0;
 }
